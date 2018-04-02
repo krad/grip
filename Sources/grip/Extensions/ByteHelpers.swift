494acc85843bae5d00676e38fe3f559c6390afd3
krad <iam@krad.io>
2018-04-02 14:16:38 -0500

    making the byte helpers public

--- Sources/grip/Extensions/ByteHelpers.swift
+++ Sources/grip/Extensions/ByteHelpers.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-func toByteArray<T: FixedWidthInteger>(_ value: T) -> [UInt8] {
+public func toByteArray<T: FixedWidthInteger>(_ value: T) -> [UInt8] {
     var bigEndian = value.bigEndian
     let count = MemoryLayout<T>.size
     let bytePtr = withUnsafePointer(to: &bigEndian) {
@@ -11,27 +11,27 @@ func toByteArray<T: FixedWidthInteger>(_ value: T) -> [UInt8] {
     return Array(bytePtr)
 }
 
-func fromByteArray<T>(_ value: [UInt8]) -> T {
+public func fromByteArray<T>(_ value: [UInt8]) -> T {
     return value.reversed().withUnsafeBytes {
         $0.baseAddress!.load(as: T.self)
     }
 }
 
-extension Data {
+public extension Data {
     
-    var bytes: [UInt8] {
+    public var bytes: [UInt8] {
         var bytes = [UInt8](repeating: 0, count: self.count)
         self.copyBytes(to: &bytes, count: self.count)
         return bytes
     }
     
-    func hexEncodedString() -> String {
+    public func hexEncodedString() -> String {
         return map { String(format: "%02hhx", $0) }.joined()
     }
 }
 
 extension UInt8 {
-    func toBool() -> Bool {
+    public func toBool() -> Bool {
         switch self {
         case 0x01:
             return true
